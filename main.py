8cdf0300151724001852a3917cae70a923c6a87b
RiyadhBenhallou <157654216+RiyadhBenhallou@users.noreply.github.com>
2024-08-19 17:58:18 +0000

    added search functionality and improved error handling

--- main.py
+++ main.py
@@ -3,24 +3,43 @@ from tkinter import messagebox
 import pyperclip
 import string
 from random import choice
+import json
 
 
 # Logic
 
 def save_pw():
-  website = website_input.get()
+  website = website_input.get().capitalize()
   email = email_input.get()
   password = password_input.get()
+  new_data = {
+    website: {
+      'email': email,
+      'password': password
+    }
+  }
+  
   if not website or not email or not password:
     messagebox.showinfo(title='Oops!', message='You left some fields empty')
   else:
     is_ok = messagebox.askokcancel(title='Confirm Save', message='Do you want to confirm saving these info?')
     if is_ok:
-      with open('data.txt', mode='a') as file:
-        file.write(f'{website} | {email} | {password}\n')
-      website_input.delete(0, tk.END)
-      email_input.delete(0, tk.END)
-      password_input.delete(0, tk.END)
+      
+      try:
+        with open('data.json', mode='r') as file:
+          data = json.load(file)
+      except FileNotFoundError:
+          with open('data.json', 'w') as file:
+            json.dump(new_data, file, indent=4)
+      else:
+        data.update(new_data)
+        with open('data.json', mode='w') as data_file:
+          json.dump(data, data_file, indent=4)
+      finally:
+        website_input.delete(0, tk.END)
+        email_input.delete(0, tk.END)
+        password_input.delete(0, tk.END)
+  
 
 def generate_pw():
   chars = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
@@ -29,7 +48,23 @@ def generate_pw():
   password_input.insert(0, password)
   pyperclip.copy(password)
   messagebox.showinfo(title='Info', message='Password copied')
+
+def search():
+  website = website_input.get().capitalize()
+  try:
+    with open('data.json') as file:
+      data = json.load(file)
+  except FileNotFoundError:
+    messagebox.showinfo(title='Oops!', message='The database is empty for now')
+  else:
+      try:
+        result = data[website]
+        messagebox.showinfo(title=f'{website}', message=f"Email: {result['email']}\nPassword: {result['password']}")
+      except KeyError:
+        messagebox.showinfo(title='Oops!', message=f'No account was found for {website}')
+
 # 
+
 
 window = tk.Tk()
 window.title('MyPass')
@@ -42,9 +77,11 @@ canvas.grid(column=1, row=0)
 
 website_label = tk.Label(text='Website:')
 website_label.grid(column=0, row=1)
-website_input = tk.Entry(width=35)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input = tk.Entry(width=21)
+website_input.grid(column=1, row=1)
 website_input.focus()
+search = tk.Button(text='Search', command=search, width=10)
+search.grid(column=2, row=1, columnspan=2)
 
 email_label = tk.Label(text='Email:')
 email_label.grid(column=0, row=2)
